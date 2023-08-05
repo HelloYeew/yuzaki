72e1dc95c694dca0d634e45bc354d619e2ebc7ae
Phawit Pornwattanakul <me@helloyeew.dev>
2023-08-06 05:11:07 +0700

    Set window title

--- Yuzaki.Desktop/YuzakiGameDesktop.cs
+++ Yuzaki.Desktop/YuzakiGameDesktop.cs
@@ -1,9 +1,21 @@
-﻿using Yuzaki.Game;
+﻿using osu.Framework.Development;
+using osu.Framework.Platform;
+using Yuzaki.Game;
 
 namespace Yuzaki.Desktop;
 
 public partial class YuzakiGameDesktop : YuzakiGame
 {
+    public override void SetHost(GameHost host)
+    {
+        base.SetHost(host);
+        var desktopWindow = host.Window;
+
+        desktopWindow.Title = "Yuzaki";
+        if (DebugUtils.IsDebugBuild)
+            desktopWindow.Title += " development";
+    }
+
     protected override void LoadComplete()
     {
         base.LoadComplete();
